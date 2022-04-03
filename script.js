e152aa9bad0af717c32927e89033b04f992bef43
Daniel Gomes <96587031+danielbgmg@users.noreply.github.com>
2022-04-03 12:46:18 -0300

    teste.

---
+++ script.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('.c-header__nav')
+const menu = document.querySelector('.c-header__icon')
+
+menu.addEventListener('click', () => {
+    nav.classList.toggle('teste')
+})
